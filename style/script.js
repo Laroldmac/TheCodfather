edac9c903a50775273930396b20bbf8ee5c01b16
Laroldmac <148687199+Laroldmac@users.noreply.github.com>
2023-11-11 23:00:56 +0000

    Update script.js

--- style/script.js
+++ style/script.js
@@ -1,18 +1,19 @@
-function myForm() {
-            // Get the form elements by their IDs
-            var form = document.getElementById('myForm');
-            var confirmationMessage = document.getElementById('confirmationMessage');
+nction myForm() {
+    // Get the form elements by their IDs
+    var form = document.getElementById('myForm');
+    var confirmationMessage = document.getElementById('confirmationMessage');
 
-            // Get the values from the form
-            var name = document.getElementById('name').value;
-            var date = document.getElementById('date').value;
-            var time = document.getElementById('time').value;
+    // Get the values from the form
+    var name = document.getElementById('name').value;
+    var date = document.getElementById('date').value;
+    var time = document.getElementById('time').value;
 
-            // Set the display property of the form to "none" to hide it
-            form.style.display = 'none';
-            // Display the confirmation message
-            confirmationMessage.innerHTML = 'Thank you, ' + name + ', your table is confirmed for ' + date + ' at ' + time;
+    // Set the display property of the form to "none" to hide it
+    form.style.display = 'none';
 
-            return false;
+    // Display the confirmation message
+    confirmationMessage.innerHTML = 'Thank you, ' + name + ', your table is confirmed for ' + date + ' at ' + time;
 
-        }
+    // Prevent default form submission
+    return false;
+}
