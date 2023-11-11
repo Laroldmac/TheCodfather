506b4361d1edaef92c4c8d8e041d353098829fe2
Laroldmac <148687199+Laroldmac@users.noreply.github.com>
2023-11-11 19:13:26 +0000

    Update script.js

--- style/script.js
+++ style/script.js
@@ -1,3 +1,7 @@
-function check(){
+function hideForm() {
+            // Get the form element by its ID
+            var form = document.querySelector('form');
 
-}
+            // Set the display property to "none" to hide the form
+            form.style.display = ('Thank you, '+name+', your table is confirmed for '+date+' at '+time);
+        }
