7da96963d01ade3579119865e03c1a1fd1786ee7
Laroldmac <148687199+Laroldmac@users.noreply.github.com>
2023-11-13 20:15:37 +0000

    Update script.js

--- style/script.js
+++ style/script.js
@@ -15,7 +15,7 @@ if (day < 10) {
 
     document.getElementById('date').min = formattedDate;
 
-document.getElementById("time").step = "900";
+document.getElementById("time").step = "900" !important;
 
 function myForm() {
     // Get the form elements by their IDs
