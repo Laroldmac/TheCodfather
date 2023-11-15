7fd1cc455a5362dacb9180f96b001466d08a7e73
Laroldmac <148687199+Laroldmac@users.noreply.github.com>
2023-11-15 19:11:35 +0000

    Update script.js

--- style/script.js
+++ style/script.js
@@ -1,41 +1,41 @@
 var currentDate = new Date();
-    var day = currentDate.getDate();
-    var month = currentDate.getMonth() + 1;
-    var year = currentDate.getFullYear();
+var day = currentDate.getDate();
+var month = currentDate.getMonth() + 1;
+var year = currentDate.getFullYear();
 
 if (day < 10) {
-        day = '0' + day; 
-    }
+    day = '0' + day; 
+}
 
-    if (month < 10) {
-        month = '0' + month; 
-    }
+if (month < 10) {
+    month = '0' + month; 
+}
 
-    var formattedDate = year + '-' + month + '-' + day;
+var formattedDate = year + '-' + month + '-' + day;
 
-    document.getElementById('date').min = formattedDate;
-
+document.getElementById('date').min = formattedDate;
 document.getElementById("time").step = "900";
-const timeInput = document.getElementById('time');
 
-timeInput.addEventListener('input', function() {
-    const selectElement = document.querySelector('#timeInput::-webkit-calendar-picker-indicator');
+// Function to filter time options based on hours and minutes
+function filterTimeOptions() {
+    const selectedHour = document.getElementById('hours').value;
+    const selectedMinute = document.getElementById('mins').value;
 
-    if (selectElement) {
-        selectElement.addEventListener('click', function() {
-            const options = document.querySelectorAll('#select option');
+    const options = document.querySelectorAll('#mins option');
 
-            options.forEach(option => {
-                const value = parseInt(option.value.split(':')[1]);
-                if (![0, 15, 30, 45].includes(value)) {
-                    option.setAttribute('hidden', 'true');
-                } else {
-                    option.removeAttribute('hidden');
-                }
-            });
-        });
-    }
-});
+    options.forEach(option => {
+        const value = option.value;
+        if ((selectedHour === '--' && value !== '--') || (selectedHour !== '--' && value !== selectedMinute)) {
+            option.setAttribute('hidden', 'true');
+        } else {
+            option.removeAttribute('hidden');
+        }
+    });
+}
+
+// Event listeners for time inputs
+document.getElementById('hours').addEventListener('change', filterTimeOptions);
+document.getElementById('mins').addEventListener('change', filterTimeOptions);
 
 function myForm() {
     // Get the form elements by their IDs
